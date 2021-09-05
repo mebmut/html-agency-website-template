b5a72cf85d3fc2100ab28c11d7b1f3b21c2531ab
mebmut <mebmut@gmail.com>
2021-09-05 21:36:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector('.toggle')
+const nav = document.querySelector('.navigation')
+
+toggle.addEventListener('click', () =>{
+    toggle.classList.toggle('active');
+    nav.classList.toggle('active');
+})
